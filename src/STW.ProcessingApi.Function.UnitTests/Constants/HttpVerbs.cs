12c1d79ab6171267d462b25e3d5ae0d2e75fa14a
Sean Treanor <150141613+sean-treanor@users.noreply.github.com>
2024-04-30 10:26:38 +0100

    Adding intial skeleton logic

---
+++ src/STW.ProcessingApi.Function.UnitTests/Constants/HttpVerbs.cs
@@ -0,0 +1,6 @@
+namespace STW.ProcessingApi.UnitTests.Constants;
+
+public static class HttpVerbs
+{
+    public const string Post = "post";
+}
---
+++ src/STW.ProcessingApi.Function.UnitTests/ProcessingApiTriggerTests.cs
@@ -0,0 +1,46 @@
+namespace STW.ProcessingApi.UnitTests;
+
+using System.Net;
+using System.Text;
+using Constants;
+using FluentAssertions;
+using Function;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TestDoubles;
+
+[TestClass]
+public class ProcessingApiTriggerTests
+{
+    private Mock<ILogger<ProcessingApiTrigger>> _logger;
+    private ProcessingApiTrigger _systemUnderTest;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _logger = new Mock<ILogger<ProcessingApiTrigger>>();
+        _systemUnderTest = new ProcessingApiTrigger(_logger.Object);
+    }
+
+    [TestMethod]
+    public void Run_WhenCalled_ReturnsRequestBodyAndRequestContentTypeHeaderInResponse()
+    {
+        // Arrange
+        const string contentType = "application/json; charset=utf-8";
+        var requestBody = new MemoryStream(Encoding.Default.GetBytes("{}"));
+        var httpRequestData = new MockHttpRequestData(Mock.Of<FunctionContext>(), requestBody, HttpVerbs.Post);
+        httpRequestData.Headers.Add(HttpHeaders.ContentType, contentType);
+
+        // Act
+        var result = _systemUnderTest.Run(httpRequestData);
+
+        // Assert
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+        result.Headers.Should().ContainKey(HttpHeaders.ContentType).WhoseValue.Should().Contain(contentType);
+        result.Body.Should().BeSameAs(requestBody);
+
+        _logger.VerifyLog(x => x.LogInformation("ProcessingApiTrigger function was invoked."));
+    }
+}
